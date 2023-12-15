d9b23382d1a67b96b52721842716030cf635c53c
zjp255 <95677971+zjp255@users.noreply.github.com>
2023-12-15 23:46:28 +0800

    2023 12 15

---
+++ LeetCode/_Q30.cpp
@@ -0,0 +1,38 @@
+// 给定一个字符串 s 和一个字符串数组 words。 words 中所有字符串 长度相同。
+
+//  s 中的 串联子串 是指一个包含  words 中所有字符串以任意顺序排列连接起来的子串。
+
+// 例如，如果 words = ["ab","cd","ef"]， 那么 "abcdef"， "abefcd"，"cdabef"， "cdefab"，"efabcd"， 和 "efcdab" 都是串联子串。 
+
+// "acdbef" 不是串联子串，因为他不是任何 words 排列的连接。
+
+// 返回所有串联子串在 s 中的开始索引。你可以以 任意顺序 返回答案。
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string,bool> hashMap;
+        int wordsLen = words.size();
+        int sLen = s.length();
+        for(int i = 0; i < wordsLen; i++)
+        {
+            hashMap[words[i]] = true;
+        }
+
+        for(int i = 0; i < sLen; i++)
+        {
+            unordered_map<string,bool> temp = hashMap;
+            string tempS = s.substr(i,3);
+            if(temp[tempS] == true)
+            {
+                
+            }
+        }
+        
+    }
+};
