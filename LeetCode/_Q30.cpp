59cc705020128d42f5a2a27e3f3151383e6055b3
zjp255 <95677971+zjp255@users.noreply.github.com>
2023-12-17 19:23:40 +0800

    2023 12 17

--- LeetCode/_Q30.cpp
+++ LeetCode/_Q30.cpp
@@ -13,10 +13,13 @@
 #include <queue>
 using namespace std;
 
+
+//超时
 class Solution {
 public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        unordered_map<string,bool> hashMap;
+    vector<int> findSubstring(string s, vector<string> words) {
+        unordered_map<string,int> hashMap;
+        unordered_map<string,unordered_map<int,int>> nextMap;
         int wordsLen = words.size();
         int sLen = s.length();
         int wLen = words.size();
@@ -24,31 +27,123 @@ public:
         vector<int> ans;
         for(int i = 0; i < wordsLen; i++)
         {
-            hashMap[words[i]] = true;
+            hashMap[words[i]] += 1;
         }
+
+        for(int i = 0 ; i < wLen; i++)
+        {
+            int index1 = 1;
+            int index2 = 0;
+            int index3 = 0;
+            while(index2 < wLen)
+            {
+                if(index2 == i)
+                {
+                    index2++;
+                    continue;
+                }
+                if(words[i][index1] == words[index2][index3])
+                {
+                    index1++;
+                    index3++;
+                    if(index1 == wordLen)
+                    {
+                        nextMap[words[i]][index1 - index3] = index2;
+                        index1 = 1;
+                        index3 = 0;
+                    }
+                }
+                else{
+                    index1 += 1;
+                    index3 = 0;
+                    if(index1 == wordLen)
+                    {
+                        index1 = 1;
+                    }
+                }
+            }   
+
+        }
+
 
         for(int i = 0; i < sLen; i++)
         {
-            unordered_map<string,bool> temp = hashMap;
-            
+            unordered_map<string,int> temp = hashMap;   
             for(int j = 0; j < wLen; j++)
             {
                 string tempS = s.substr(i + j * wordLen, wordLen);
-                if(temp[tempS] == true)
+                if(temp[tempS] > 0)
                 {
-                    temp[tempS] = false;
-                    if(j == sLen - 1)
+                    temp[tempS] -= 1;
+                    if(j == wLen - 1)
                     {
                         ans.push_back(i);
                     }
                 }
                 else{
+                    
                     break;
                 }
-            }
-            
+            }     
         }
-        return ans;
-        
+        return ans;        
     }
 };
+
+
+
+//滑动窗口法
+//68ms 67.41% 38MB 24.60%
+class Solution {
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+        unordered_map<string,int> wordCount;
+        int sLen = s.length();
+        int wLen = words.size();
+        int wordLen = words[0].length();
+        int windowSize = wLen * wordLen;
+        vector<int> ans;
+        for(int i = 0; i < wLen; i++)
+        {
+            wordCount[words[i]] -= 1;
+        }
+
+        for(int i = 0; i < wordLen; i++)
+        {
+            unordered_map<string,int> tempWordCount = wordCount;
+            for(int j = 0; j < wLen; j++)
+            {
+                string temp = s.substr(i + j * wordLen,wordLen);
+                if(++tempWordCount[temp] == 0)
+                {
+                    tempWordCount.erase(temp);
+                }
+            }
+            for(int j = i; j <= sLen - windowSize; j += wordLen)
+            {
+                if(tempWordCount.empty())
+                {
+                    ans.push_back(j);
+                }
+                string temp = s.substr(j,wordLen);
+                if(--tempWordCount[temp] == 0)
+                {
+                    tempWordCount.erase(temp);
+                }
+                temp = s.substr(j + windowSize,wordLen);
+                if(++tempWordCount[temp] == 0)
+                {
+                    tempWordCount.erase(temp);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<string> temp = {"foo","bar"};
+    s.findSubstring("barfoothefoobarman", temp);
+}
