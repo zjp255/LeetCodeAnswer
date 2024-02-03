02066e2367ced9e7006df3cf5352d42cccd7c87f
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-02-03 23:52:25 +0800

    2024 2 3

---
+++ LeetCode/_Q78.cpp
@@ -0,0 +1,25 @@
+// 给你一个整数数组 nums ，数组中的元素 互不相同 。返回该数组所有可能的子集（幂集）。
+
+// 解集 不能 包含重复的子集。你可以按 任意顺序 返回解集。
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> part;
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+    }
+
+    void combine(vector<int>& nums,int start,int targetLen,int len)
+    {
+        for(int i = start; i <= nums.size(); i++)
+        {
+            
+        }
+    }
+
+};
