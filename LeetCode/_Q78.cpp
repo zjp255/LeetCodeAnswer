cc489e3c806ebeebf0056b1b86cee4e19ab3d38c
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-02-04 00:05:54 +0800

    2024 2 4

--- LeetCode/_Q78.cpp
+++ LeetCode/_Q78.cpp
@@ -6,19 +6,31 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+//0ms 100% 8.26Mb 16.8%
 class Solution {
 public:
     vector<vector<int>> ans;
     vector<int> part;
     vector<vector<int>> subsets(vector<int>& nums) {
-
+        for(int i = 0; i <= nums.size(); i++)
+        {
+            combine(nums,0,i);
+        }
+        return ans;
     }
 
-    void combine(vector<int>& nums,int start,int targetLen,int len)
+    void combine(vector<int>& nums,int start,int targetLen)
     {
-        for(int i = start; i <= nums.size(); i++)
+        if(part.size() == targetLen)
         {
-            
+            ans.push_back(part);
+        }else{
+            for(int i = start; i <= nums.size(); i++)
+            {
+                part.push_back(nums[i]);
+                combine(nums,i + 1,targetLen);
+                part.pop_back();
+            }
         }
     }
 
