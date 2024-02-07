a6c78f5a01044727c1422108a842034dda8a9d45
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-02-07 21:32:56 +0800

    2024 2 7

---
+++ LeetCode/_Q47.cpp
@@ -0,0 +1,41 @@
+// 给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列
+
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+//0ms 100% 11.1MB 26.2%
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> part;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<bool> used(nums.size(),false);
+        combine(nums,used);
+        return ans;
+    }
+
+    void combine(vector<int>& nums,vector<bool>& used)
+    {
+        if(part.size() == nums.size())
+        {
+            ans.push_back(part);
+        }else{
+            int usedNum[21] = {0};
+            for(int i = 0; i < nums.size(); i++)
+            {
+                if(used[i] == true || usedNum[nums[i] + 10] == 1)
+                {
+                    continue;
+                }
+                usedNum[nums[i] + 10] = 1;
+                part.push_back(nums[i]);
+                used[i] = true;
+                combine(nums,used);
+                used[i] = false;
+                part.pop_back();
+            }
+        }
+    }
+};
