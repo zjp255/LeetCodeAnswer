0635058ef4fe31d6b11b64834d6a9e3a8f27bf09
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-02-24 18:45:19 +0800

    2024 2 24

---
+++ LeetCode/_Q406.cpp
@@ -0,0 +1,55 @@
+// 假设有打乱顺序的一群人站成一个队列，数组 people 表示队列中一些人的属性（不一定按顺序）。每个 people[i] = [hi, ki] 表示第 i 个人的身高为 hi ，前面 正好 有 ki 个身高大于或等于 hi 的人。
+
+// 请你重新构造并返回输入数组 people 所表示的队列。返回的队列应该格式化为数组 queue ，其中 queue[j] = [hj, kj] 是队列中第 j 个人的属性（queue[0] 是排在队列前面的人）。
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+using namespace std;
+//99ms 41.93% 27.95MB 7.15%
+class Solution {
+public:
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(),people.end(),cmp_secend);
+        vector<vector<int>> ans;
+        ans.push_back(people[0]);
+        for(int i = 1; i < people.size(); i++)
+        {
+            int count = 0;
+            for(int j = 0; j < ans.size(); j++)
+            {
+                if(ans[j][0] >= people[i][0])
+                {
+                    count++;
+                }
+                 if(count == people[i][1])
+                {
+                    if(j + 1 < ans.size() && people[i][0] > ans[j + 1][0])
+                        continue;
+                    ans.insert(ans.begin() + j + 1,people[i]);
+                    break;
+                }   
+            }
+        }
+        return ans;
+    }
+
+    static bool cmp_secend(vector<int> x,vector<int> y)
+    {
+        if(x[1] != y[1])
+        {
+            return x[1] < y[1];
+        }else{
+            return x[0] < y[0];
+        }
+    }
+};
+
+int main()
+{
+    Solution s;
+     vector<vector<int>> temp = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+    s.reconstructQueue(temp);
+
+}
