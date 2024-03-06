356888d60a29343db076131fcc232887e63aa147
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-06 20:10:54 +0800

    2024 3 6

---
+++ LeetCode/_Q416.cpp
@@ -0,0 +1,15 @@
+// 给你一个 只包含正整数 的 非空 数组 nums 。
+// 请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include <list>
+using namespace std;
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        
+    }
+};
