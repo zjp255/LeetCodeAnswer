d3a59657450b33697fb52b5162d8df25441c0a67
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-10 15:50:24 +0800

    2024 3 10

--- LeetCode/_Q494.cpp
+++ LeetCode/_Q494.cpp
@@ -10,10 +10,41 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
-
+//4ms 74% 13.89 MB 14%
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        
+         //target = x - sum + x
+        //x = (target + sum)/2
+        //dp[i][j] 1 ~ i 任意加减 得到j的方式,j指的是添加的大小
+        //dp[i][j] = dp[i - 1][j - nums[i]]
+        int sum = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
+        }
+        int jSize = target + sum;
+        if(abs(target) > sum || jSize % 2 == 1)
+        {
+            return 0;
+        }
+        jSize /= 2;
+        vector<vector<int>> dp(nums.size() + 1,vector<int>(jSize + 1));
+        for(int i = 0 ; i <= nums.size(); i++)
+        {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= nums.size(); i++)
+        {
+            for (int j = 0; j <= jSize; j++)
+            {
+                if(j - nums[i - 1] >= 0)
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                else
+                dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[nums.size()][jSize];
     }
 };
