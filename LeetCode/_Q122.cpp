a0ef635aa805082f707e5891ee590304f3be2bf7
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-21 14:15:58 +0800

    2024 3 21

--- LeetCode/_Q122.cpp
+++ LeetCode/_Q122.cpp
@@ -9,6 +9,31 @@
 #include <queue>
 using namespace std;
 
+//动规
+//3ms 86.85% 15MB 28%
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        //dp[i][0]表示第i天持有这支股票最大利润
+        //dp[i][1]表示第i天不持有这只股票的最大利润
+        //dp[i][0] = max(dp[i - 1][0],dp[i - 1][0] - prices[i])
+        //dp[i][1] = max(dp[i - 1][1],dp[i - 1][0] + prices[i])
+        vector<vector<int>> dp(2,vector<int>(2));
+        dp[0][0] -= prices[0];
+        for(int i = 1; i < prices.size();i++)
+        {
+            dp[1][0] = max(dp[0][0],dp[0][1] - prices[i]);
+            dp[1][1] = max(dp[0][1],dp[0][0] + prices[i]);
+            dp[0][0] = dp[1][0];
+            dp[0][1] = dp[1][1];
+        }
+        return max(dp[1][0],dp[1][1]);
+    }
+};
+
+
+
+//贪心算法
 //0ms 100% 15.10MB 37.67%
 class Solution {
 public:
