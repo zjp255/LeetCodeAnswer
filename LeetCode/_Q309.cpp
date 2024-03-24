7ba2eb0d4ead204777823b75733bd019ce1cf834
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-24 13:06:45 +0800

    2024 3 24

---
+++ LeetCode/_Q309.cpp
@@ -0,0 +1,29 @@
+// 给定一个整数数组prices，其中第  prices[i] 表示第 i 天的股票价格 。​
+
+// 设计一个算法计算出最大利润。在满足以下约束条件下，你可以尽可能地完成更多的交易（多次买卖一支股票）:
+
+// 卖出股票后，你无法在第二天买入股票 (即冷冻期为 1 天)。
+// 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+//2ms 74% 14.7MB 50%
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if (n == 0) return 0;
+        vector<vector<int>> dp(n, vector<int>(4, 0));
+        dp[0][0] -= prices[0]; // 持股票
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]));
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);
+            dp[i][2] = dp[i - 1][0] + prices[i];
+            dp[i][3] = dp[i - 1][2];
+        }
+        return max(dp[n - 1][3], max(dp[n - 1][1], dp[n - 1][2]));
+    }
+};
