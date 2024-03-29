cbe0ed34bc58fa4d0ac7c3ba6b05fb4cd58c2a3a
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-29 12:55:05 +0800

    2024 3 29

---
+++ LeetCode/_Q1143.cpp
@@ -0,0 +1,30 @@
+// 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+
+// 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+
+// 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+// 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+//24ms 67% 25MB 50%
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size() + 1, vector<int>(text2.size() + 1));
+        for (int i = 1; i <= text1.size(); i++)
+        {
+            for (int j = 1; j <= text2.size(); j++)
+            {
+                if(text1[i - 1] == text2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);  
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
+};
