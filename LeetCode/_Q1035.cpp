58fed8ee8d2bba9a9855f57f592c72e32e55dd06
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-30 22:07:46 +0800

    2024 3 30

---
+++ LeetCode/_Q1035.cpp
@@ -0,0 +1,22 @@
+// 在两条独立的水平线上按给定的顺序写下 nums1 和 nums2 中的整数。
+
+// 现在，可以绘制一些连接两个数字 nums1[i] 和 nums2[j] 的直线，这些直线需要同时满足：
+
+//  nums1[i] == nums2[j]
+// 且绘制的直线不与任何其他连线（非水平线）相交。
+// 请注意，连线即使在端点也不能相交：每个数字只能属于一条连线。
+
+// 以这种方法绘制线条，并返回可以绘制的最大连线数。
+
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+
+    }
+};
