87abe42b9ae573457292dcb957ebb3b0b402d3b4
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-03-31 14:40:14 +0800

    2024 3 31

--- LeetCode/_Q1035.cpp
+++ LeetCode/_Q1035.cpp
@@ -13,10 +13,25 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
-
+//3ms 95% 15MB 52%
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-
+        //dp[i][j]到i时的最大条数
+        //if nums[i] == nums[j] dp[i][j] = dp[i - 1][j - 1] + 1
+        //else dp[i][j] max(dp[i][j - 1],dp[i - 1][j])
+        vector<vector<int>> dp(nums1.size() + 1,vector<int>(nums2.size() + 1));
+        for (int i = 1; i <= nums1.size(); i++)
+        {
+            for (int j = 1; j <= nums2.size(); j++)
+            {
+                if(nums1[i - 1] == nums2[j - 1])
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = max(dp[i][j - 1],dp[i - 1][j]);
+            }
+            
+        }
+        return dp[nums1.size()][nums2.size()];
     }
 };
