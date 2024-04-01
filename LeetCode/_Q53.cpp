e327e71bd950bcb59b2c2fc5a547dfc057c1419d
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-04-01 14:41:57 +0800

    2024 4 1

--- LeetCode/_Q53.cpp
+++ LeetCode/_Q53.cpp
@@ -6,6 +6,25 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+//动态规划
+//70 ms 90% 70MB 22%
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for (int i = 1; i < nums.size(); i++)
+        {
+            dp[i] = max(dp[i - 1] + nums[i],nums[i]);
+            if(dp[i] > ans)
+                ans = dp[i];
+        }
+        return ans;
+    }
+};
+
+
 //82ms 77% 68.57MB 40%
 class Solution {
 public:
