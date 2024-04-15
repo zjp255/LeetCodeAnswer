79cab03589f2cd09ec168f66248ce322c072599d
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-04-15 10:25:19 +0800

    2024 4 15

--- LeetCode/_Q200.cpp
+++ LeetCode/_Q200.cpp
@@ -10,6 +10,59 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+//41ms 10.61% 19.38mb 23%
+//广搜法
+class Solution {
+public:
+     int dir[4][2] = {0,1,1,0,-1,0,0,-1};
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size(),n = grid[0].size();
+        vector<vector<bool>> used(m,vector<bool>(n,false));
+        queue<pair<int,int>> que;
+        int ans = 0;
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if(!used[i][j] && grid[i][j] == '1')
+                {
+                    //cout<<i<<" "<<j<<" ";
+                    ans += 1;
+                    que.push({i,j});
+                    dfs(grid,used,que);
+                }
+            }
+        }
+        return ans;
+    }
+
+    void dfs(vector<vector<char>>& grid,vector<vector<bool>>& used, queue<pair<int,int>>& que)
+    {
+        while (!que.empty())
+        {
+            int x = que.front().first,y = que.front().second;
+            que.pop();
+            if(used[x][y] || grid[x][y] == '0')
+                continue;
+            used[x][y] = true;
+            for(int i = 0; i < 4; i++)
+            {
+                int tx = x + dir[i][0];
+                int ty = y + dir[i][1];
+                if( tx < 0 || tx >= grid.size() || ty < 0 || ty >= grid[0].size())
+                    continue;
+                if(!used[tx][ty])
+                {
+                    que.push({tx,ty});
+                }
+            }
+        }
+        
+    }
+};
+
+
+
 //37ms 19.68% 15.74Mb 45%
 //深搜法
 class Solution {
