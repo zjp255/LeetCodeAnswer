e198f4340465019b3f7d9bda77d180f527bd098b
zjp255 <95677971+zjp255@users.noreply.github.com>
2024-05-16 14:33:49 +0800

    2024 5 16

--- LeetCode/_Q15.cpp
+++ LeetCode/_Q15.cpp
@@ -6,8 +6,52 @@
 #include<iostream>
 #include<vector>
 #include <unordered_map>
-//超时
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
+//哈希表
+//1539ms 5% 436.57mb 5%
+class Solution1 {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+         vector<vector<int>> result;
+        sort(nums.begin(),nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] > 0)
+            {
+                break;
+            }
+
+            if(i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+            unordered_set<int> set;
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if(j > i + 2 && nums[j] == nums[j - 1]
+                && nums[j - 1] == nums[j - 2])
+                    continue;
+                
+                int c = 0 - nums[i] - nums[j];
+                if(set.find(c) != set.end())
+                {
+                    result.push_back({nums[i],nums[j],c});
+                    set.erase(c);
+                }
+                else{
+                    set.insert(nums[j]);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+//超时
+
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
