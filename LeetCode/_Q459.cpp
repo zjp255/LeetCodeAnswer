759721b4f9c8fee629501b3c8462f35d95f0d0df
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-05-24 10:19:19 +0800

    2024 5 24

--- LeetCode/_Q459.cpp
+++ LeetCode/_Q459.cpp
@@ -6,6 +6,41 @@
 #include <map>
 #include <queue>
 using namespace std;
+//KMP
+//15ms 72.76% 13.64MB 25.35%
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        vector<int> next = kmp(s);
+        
+        int len = s.length();
+
+        if(next[len - 1] != 0 && len % (len - next[len - 1]) == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    vector<int> kmp(string& s)
+    {
+        vector<int> next(s.length(),0);
+        int index = 0;
+        for (int i = 1; i < s.length(); i++)
+        {
+            while(index > 0 && s[i] != s[index])
+                index = next[index - 1];
+            if(s[i] == s[index])
+            {
+                next[i] = index + 1;
+                index++;
+            }
+        }
+        return next;
+    }
+};
+
+
 //13ms 88.53% 12.83MB 50.96%
 class Solution {
 public:
