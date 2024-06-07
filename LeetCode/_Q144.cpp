4041822e7462cb039376b7a9f8a8336d09f2c4ee
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-06-07 16:02:49 +0800

    2024 6 7

--- LeetCode/_Q144.cpp
+++ LeetCode/_Q144.cpp
@@ -4,6 +4,7 @@
 #include<vector>
 #include <unordered_map>
 #include <queue>
+#include <stack>
 #include <unordered_set>
 using namespace std;
 
@@ -17,6 +18,31 @@ struct TreeNode {
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
+//0ms 100% 9.63MB 72.24%
+class Solution1 {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> ans;
+        if(root == NULL) return ans;
+        st.push(root);
+
+        while(!st.empty())
+        {
+            TreeNode* temp = st.top();
+            ans.push_back(temp->val);
+            st.pop();
+            if(temp->right != NULL)
+                st.push(temp->right);
+            if(temp->left != NULL)
+                st.push(temp->left);
+        }
+        return ans;
+    }
+};
+
+
+
   //0ms 100% 9.7MB 35.96%
  class Solution {
 public:
