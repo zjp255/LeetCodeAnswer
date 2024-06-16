e916dd48438ff76ee56ed6375cd1ed0ae2dea7e6
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-06-16 14:56:43 +0800

    2024 6 16

---
+++ LeetCode/_Q104.cpp
@@ -0,0 +1,44 @@
+// 给定一个二叉树 root ，返回其最大深度。
+
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+//6ms 64.79% 17.10MB 50%
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        queue<TreeNode*> que;
+        if(root) que.push(root);
+        while(!que.empty())
+        {
+            int size = que.size();
+            depth++;
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* temp = que.front();
+                que.pop();
+                if(temp->left) que.push(temp->left);
+                if(temp->right) que.push(temp->right);
+            }
+        }
+        return depth;
+    }
+};
