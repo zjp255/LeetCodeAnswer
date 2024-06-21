370c41f7e6aebdb19b0e7a6f7f57bcdcbc6e46fa
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-06-21 22:24:21 +0800

    2024 6 21

---
+++ LeetCode/_Q101.cpp
@@ -0,0 +1,48 @@
+// 给你一个二叉树的根节点 root ， 检查它是否轴对称。
+#include<iostream>
+#include<vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+  struct TreeNode {
+    int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        queue<TreeNode*> que;
+        if(root) que.push(root);
+        else
+            return false;
+        int lastLayerCount = 1;
+        while (!que.empty())
+        {
+            int size = que.size();
+            
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode* temp = que.front();
+                que.pop();
+                if(temp->left) 
+                {
+                    que.push(temp->left);
+                    lastLayerCount++;
+                }
+                if(temp->right)
+                {
+                    que.push(temp->right);
+                    lastLayerCount++;
+                }
+                if(lastLayerCount != 1 && lastLayerCount * 2 != size)
+                    return false;
+            }
+        }
+        return true;
+    }
+};
