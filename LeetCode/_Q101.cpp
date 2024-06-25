f4d03982822e6605fc68236c65675196e22c9d88
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-06-25 23:54:50 +0800

    2024 6 25

--- LeetCode/_Q101.cpp
+++ LeetCode/_Q101.cpp
@@ -15,13 +15,46 @@ using namespace std;
  //3ms 75% 16.94MB 68.27%
 class Solution {
 public:
-      bool check(TreeNode* left ,TreeNode* right)
+    bool check(TreeNode* left ,TreeNode* right)
     {
         if(!left && !right)
             return true;
         if(!left || !right)
             return false;
         return left->val == right->val && check(left->left,right->right) && check(left->right, right->left); 
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
+
+
+//迭代
+//0ms 100% 17.3MB 11%
+class Solution {
+public:
+    bool check(TreeNode* left ,TreeNode* right)
+    {
+        queue<TreeNode*> que;
+        que.push(left);
+        que.push(right);
+        while(!que.empty())
+        {
+            left = que.front(); que.pop();
+            right = que.front(); que.pop();
+            if(left == NULL && right == NULL)
+                continue;
+            if ((!left || !right) || (left->val != right->val)) return false;
+
+            que.push(left->left);
+            que.push(right->right);
+
+            que.push(left->right);
+            que.push(right->left);
+
+        }
+        return true;
     }
 
     bool isSymmetric(TreeNode* root) {
