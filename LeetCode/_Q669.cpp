33228526d27a850f96c3dad2f441ec15b91439e0
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-07-22 09:35:22 +0800

    Update _Q669.cpp
    2024 7 22

--- LeetCode/_Q669.cpp
+++ LeetCode/_Q669.cpp
@@ -19,14 +19,16 @@ struct TreeNode
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-
+//8md 83.37% 22.85MB 10.18%
 class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(root == nullptr)
             return nullptr;
-        
-
-        
+        if(root->val < low) return trimBST(root->right, low, high);
+        if(root->val > high) return trimBST(root->left, low, high);
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        return root;
     }
 };
