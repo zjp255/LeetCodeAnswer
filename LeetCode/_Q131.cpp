0e8f22e1ad7d0fc70e3c02a83bd3d4cafa325e58
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-08-03 14:37:52 +0800

    Update _Q131.cpp
    2024 8 3

--- LeetCode/_Q131.cpp
+++ LeetCode/_Q131.cpp
@@ -7,6 +7,53 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+//回溯 + 动态规划
+//96ms 82.95% 77.61MB 50.63%
+class Solution {
+public:
+    vector<vector<string>> ans;
+    vector<string> part;
+    vector<vector<bool>> dp;
+    vector<vector<string>> partition(string s) {
+        huWen(s);
+        combination(s, 0);
+        return ans;
+    }
+
+    void combination(const string& s, int start) {
+        if (start >= s.size()) {
+            ans.push_back(part);
+            return;
+        }
+        for (int i = 1; i <= s.size() - start; i++) {
+            if (!dp[start][start + i - 1]) {
+                continue;
+            }
+            string str = s.substr(start, i);
+            part.push_back(str);
+            combination(s, start + i);
+            part.pop_back();
+        }
+    }
+
+    void huWen(const string& s) {
+        int size = s.size();
+        dp = vector(size, vector<bool>(size, false));
+        // dp[i][j]表示从下标i 到 j 之间的字符串是否为回文字符串
+        // i==j dp[i][j] = true
+        // dp[i][j] = dp[i - 1]dp[j - 1] && s[i] = s[j]
+        for (int i = size - 1; i >= 0; i--) {
+            for (int j = 0; j < size; j++) {
+                if (i > j)
+                    continue;
+                if (s[i] == s[j] && (i + 1 > j - 1 || dp[i + 1][j - 1]))
+                    dp[i][j] = true;
+            }
+        }
+    }
+};
+
+
 //87ms 91.32% 77.61MB 50.31%
 class Solution {
 public:
