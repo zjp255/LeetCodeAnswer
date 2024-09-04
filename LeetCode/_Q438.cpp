9e13697b096af56ea80c9cc7607b13b9f5bd78e4
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-09-04 19:57:13 +0800

    Update _Q438.cpp

--- LeetCode/_Q438.cpp
+++ LeetCode/_Q438.cpp
@@ -10,7 +10,54 @@
 #include <queue>
 #include <list>
 using namespace std;
+//3ms 99.17% 9.91Mb 72%
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        int sLen = s.size(), pLen = p.size();
+        if (sLen < pLen)
+            return {};
+        int eff = 0;
+        int left = 0;
+        vector<int> used = vector<int>(26, -1);
+        vector<int> ans;
+        for (char temp : p) {
+            if (used[temp - 'a'] == -1)
+                used[temp - 'a'] = 0;
+            used[temp - 'a']++;
+        }
+        for (int i = 0; i < sLen; i++) {
+            if (used[s[i] - 'a'] > 0) {
+                used[s[i] - 'a']--;
+                eff++;
+            } else if (used[s[i] - 'a'] == 0) {
+                while (s[left] != s[i]) {
+                    if (used[s[left] - 'a'] != -1) {
+                        used[s[left] - 'a']++;
+                        eff--;
+                    }
+                    left++;
+                }
+                left++;
+            }
 
+            if (i - left > pLen - 1) {
+                if (used[s[left] - 'a'] != -1) {
+                    used[s[left] - 'a']++;
+                    eff--;
+                }
+                left++;
+            }
+
+            if (eff == pLen)
+                ans.push_back(left);
+        }
+        return ans;
+    }
+};
+
+
+//8ms 77.59% 11.03MB 29.82%
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
@@ -28,31 +75,25 @@ public:
                 if (myMap[s[i]] > 0) {
                     eff++;
                     myMap[s[i]]--;
-                }else{
-                    while(s[left] != s[i]){
-                        if(myMap.find(s[left]) != myMap.end()){
-                             myMap[s[left]]++;
-                             eff--;
+                } else {
+                    while (s[left] != s[i]) {
+                        if (myMap.find(s[left]) != myMap.end()) {
+                            myMap[s[left]]++;
+                            eff--;
                         }
                         left++;
                     }
-                    if(myMap.find(s[left]) != myMap.end()){
-                        myMap[s[left]]++;
-                        eff--;
-                    }
                     left++;
                 }
             }
-            else{
-                if(i - left > k - 1 && myMap.find(s[left]) != myMap.end())
-                {
+            if (i - left > k - 1) {
+                if (myMap.find(s[left]) != myMap.end()) {
                     myMap[s[left]]++;
                     eff--;
-                    left++;
                 }
+                left++;
             }
-            if (eff == k && i - left == k - 1)
-            {
+            if (eff == k && i - left == k - 1) {
                 ans.push_back(left);
             }
         }
