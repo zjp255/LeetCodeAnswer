c257cfedb52f6089a5a4b420c0601b309ced3457
‘zjp255’ <95677971+zjp255@users.noreply.github.com>
2024-09-07 12:49:43 +0800

    Update _Q239.cpp

--- LeetCode/_Q239.cpp
+++ LeetCode/_Q239.cpp
@@ -10,8 +10,32 @@
 using namespace std;
 
 
-
-
+//165ms 92.47% 134.13MB 60.56%
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+         deque<int> dq;
+        vector<int> ans;
+        for (int i = 0; i < k; i++) {
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        ans.push_back(nums[dq.front()]);
+        for (int i = k; i < nums.size(); i++) {
+            while (!dq.empty() && nums[dq.back()] <= nums[i]) {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+            while (dq.front() <= i - k) {
+                dq.pop_front();
+            }
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
 
 
 
